febfcfbb11332f77fa9abb9c6c9ded04f2bdf94a
Paras Anand <qtparas@gmail.com>
2020-08-01 22:01:23 +0530

    credentials

--- config/credentials.js
+++ config/credentials.js
@@ -1,5 +1,10 @@
 exports.credentials = {
-  username: "xyz@gmail.com",
-  escapedPassword: "xyz",
-  recipients: ["xyz@gmail.com"],
+  username: "parascurious@gmail.com",
+  escapedPassword:
+    '=$/9<^fF}qA!&[,2;\\r47Z_%sQ@{>]D?Brvv5~^E?BP5&:rsRBS9TN8(%4+y$C^#HN"*[#js(uU;=>Fbf*k/V6tw{ZSE4d][6E,p',
+  recipients: [
+    "speed1992@gmail.com",
+    // "lkssgn@gmail.com",
+    //  "rvishu35@gmail.com"
+  ],
 };
