f88d89f914802497e9baaad2a8517998ffade2ef
Paras Anand <qtparas@gmail.com>
2020-08-02 01:29:18 +0530

    added chron

--- server.js
+++ server.js
@@ -3,49 +3,51 @@ require("dotenv").config();
 const { getLatestDataFromGithub } = require("./utils/utils");
 
 const mongoose = require("mongoose");
+let cron = require("node-cron");
 
 const { sendMail } = require("./utils/mailUtils");
 const { config } = require("./config/config");
 
 const URI = process.env.MONGODB_URL;
 
-console.log("Apps@@@@@@@@@@@@@@@@@@@@Started");
-
+console.log("App@@@@@@@@@@@@@@@@@@@@Started");
 try {
-  mongoose.connect(
-    URI,
-    {
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useFindAndModify: false,
-    },
-    async (err) => {
-      if (err) {
-        console.log(
-          "@@@@@@@@@@@@@@@@@@@@@@@@@@@",
-          err,
-          "@@@@@@@@@@@@@@@@@@@@@@@"
-        );
-        throw err;
+  cron.schedule("0 * * * * *", () => {
+    mongoose.connect(
+      URI,
+      {
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+      },
+      async (err) => {
+        if (err) {
+          console.log(
+            "@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+            err,
+            "@@@@@@@@@@@@@@@@@@@@@@@"
+          );
+          throw err;
+        }
+
+        console.log("@@@@@@@@@@@@@@@@@@@MongoDB is connected@@@@@@@@@@@@@@@@@");
+
+        const response = await getLatestDataFromGithub();
+        const { status: newVersionExists } = response;
+
+        if (newVersionExists) {
+          const {
+            data: { name: version, html_url, body },
+          } = response;
+          const text = `${config.textLine2}: ${html_url}`;
+          const subject = `${config.appName} ${version} ${config.subjectPhrase}`;
+
+          config.emailFeature ? sendMail({ text, subject }) : null;
+        }
       }
-
-      console.log("@@@@@@@@@@@@@@@@@@@MongoDB is connected@@@@@@@@@@@@@@@@@");
-
-      const response = await getLatestDataFromGithub();
-      const { status: newVersionExists } = response;
-
-      if (newVersionExists) {
-        const {
-          data: { name: version, html_url, body },
-        } = response;
-        const text = `${config.textLine2}: ${html_url}`;
-        const subject = `${config.appName} ${version} ${config.subjectPhrase}`;
-
-        config.emailFeature ? sendMail({ text, subject }) : null;
-      }
-    }
-  );
+    );
+  });
 } catch (e) {
   console.log(
     "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
