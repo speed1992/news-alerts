13239b16d8e9e117372239fa79ab6ab36275e7cc
Paras <paras@paras-CNBIC-EAN1>
2021-04-14 13:08:46 +0530

    appMode config added

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-process.env.NODE_ENV = "production"
-
 require("dotenv").config()
 const cron = require("node-cron")
 
 const { logger } = require("./config/logConfig")
 const { CRA, handleFaliure } = require("./utils/utils")
 const { connectWithDatabase } = require("./utils/dbutils")
+const { config } = require("./config/config")
+
+process.env.NODE_ENV = config.appMode
+
+logger.info("App Running on " + process.env.NODE_ENV + " environment");
 
 try {
   logger.info("App started.")
