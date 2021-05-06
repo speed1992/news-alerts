3f54919d540698f7965f31004e0ae8bb358107ed
Paras Anand <qtparas@gmail.com>
2021-05-06 18:30:27 +0530

    logging updated

--- config/logConfig.js
+++ config/logConfig.js
@@ -1,5 +1,5 @@
 const { createLogger, format, transports } = require("winston")
-const { printf } = format
+const { printf, colorize } = format
 
 const myFormat = printf(({ timestamp, level, message, meta }) => {
   return `${timestamp};${level};${message};${meta ? JSON.stringify(meta) : ""}`
@@ -8,6 +8,7 @@ const myFormat = printf(({ timestamp, level, message, meta }) => {
 const logger = createLogger({
   level: "info",
   format: format.combine(
+    colorize({ all: true }),
     format.timestamp({
       format: "DD-MM-YYYY HH:mm:ss"
     }),
@@ -15,12 +16,7 @@ const logger = createLogger({
     format.splat(),
     myFormat
   ),
-  // defaultMeta: { service: "your-service-name" },
   transports: [
-    //
-    // - Write to all logs with level `info` and below to `quick-start-combined.log`.
-    // - Write all logs error (and below) to `quick-start-error.log`.
-    //
     new transports.File({ filename: "quick-start-error.log", level: "error" }),
     new transports.File({ filename: "quick-start-combined.log" })
   ]
