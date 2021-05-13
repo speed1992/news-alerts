e34fb519b97cd3dbc5ab1746a814f99b1fc3314c
Paras Anand <qtparas@gmail.com>
2021-05-13 19:37:54 +0530

    slack ping added on project start

--- server.js
+++ server.js
@@ -4,17 +4,22 @@ const { logger } = require("./config/logConfig")
 const { CRA, handleFailure } = require("./utils/utils")
 const { connectWithDatabase } = require("./utils/dbutils");
 const { attachErrorHandlers } = require("./utils/globalExceptionHandlers");
+const { slackPing } = require("./utils/slack");
+const { getDateAndTime } = require("./utils/dateUtils");
 
-logger.info("\n\n\nApp Running on " + process.env.NODE_ENV + " environment\n\n\n");
+logger.info("App started.")
+
+const appStartedMessage = "\n\n\nApp Running on " + process.env.NODE_ENV + " environment\n\n\n"
+logger.info(appStartedMessage);
 
 try {
-  logger.info("App started.")
   attachErrorHandlers();
 
   if (process.env.NODE_ENV !== "production") {
     connectWithDatabase([CRA]);
   }
   else {
+    slackPing(appStartedMessage + "at" + getDateAndTime());
     cron.schedule("0 0 * * * *", () => {
       connectWithDatabase([CRA])
     })
