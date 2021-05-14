042f3f1bdf35d5bc943b1ebbd753fa2a6ce68675
Paras Anand <qtparas@gmail.com>
2021-05-14 14:12:51 +0530

    updated slack token

--- config/credentials.js
+++ config/credentials.js
@@ -10,5 +10,5 @@ module.exports.credentials = {
     //  "rvishu35@gmail.com"
   ],
   errorRecipients: ["speed1992@gmail.com"],
-  slackToken: "xoxb-2050025082209-2034382831637-4bxo7Qej7yvVR2Mi4nm8r69f"
+  slackToken: "xoxb-2050025082209-2034382831637-1poNL1KNcSVjDwjxkVduVNIO"
 }
