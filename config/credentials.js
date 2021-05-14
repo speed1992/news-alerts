6955e1fa919f573439ef58e7e4aaf9382a7b4413
Paras Anand <qtparas@gmail.com>
2021-05-14 13:32:11 +0530

    updated gitignore

---
+++ config/credentials.js
@@ -0,0 +1,14 @@
+module.exports.credentials = {
+  username: "parascurious@gmail.com",
+  escapedPassword:
+    "hay2B93eSzC8Mg7Ahp7D5rqa3LFkqEpG3EPNVUsT7hzFg9g5T5kkWRLUGCwghQykx5anXWyYjY5sx5QzGwwVeLnhTXY2YceZTCvA",
+  recipients: [
+    "speed1992@gmail.com",
+    "parascurious@gmail.com",
+    "qtparas@gmail.com"
+    // "lkssgn@gmail.com",
+    //  "rvishu35@gmail.com"
+  ],
+  errorRecipients: ["speed1992@gmail.com"],
+  slackToken: "xoxb-2050025082209-2034382831637-4bxo7Qej7yvVR2Mi4nm8r69f"
+}
