51abdea9ff50a8203c56600beb5b2b603a51462b
Paras Anand <qtparas@gmail.com>
2023-04-22 03:54:25 +0530

    vercel

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { start } = require("../app");
-const { credentials } = require("../config/credentials");
-const { getDateAndTime } = require("../utils/dateUtils");
-const { sendMail } = require("../utils/mailUtils");
-const { slackPing } = require("../utils/slack");
+const { credentials } = require("../config/credentialss");
+const { getDateAndTime } = require("../utils/dateUtilss");
+const { sendMail } = require("../utils/mailUtilss");
+const { slackPing } = require("../utils/slackk");
 const app = express();
 const port = process.env.PORT || 5000;
 
