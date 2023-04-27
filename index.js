d382953ed75a6042a10ecb6cde71432425710351
Paras Anand <qtparas@gmail.com>
2023-04-28 01:48:42 +0530

    updated vercel config

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { start } = require("./app");
+const { credentials } = require("./config/credentials");
+const { getDateAndTime } = require("./utils/dateUtils");
+const { sendMail } = require("./utils/mailUtils");
+const { slackPing } = require("./utils/slack");
+const app = express();
+const port = process.env.PORT || 80;
+
+app.get("/health", (req, res) => {
+  const log = 'Route "/" hit. Ran successfully at ' + getDateAndTime();
+
+  // slackPing(log);
+
+  res.sendStatus(200);
+});
+
+app.get("/", (req, res) => {
+  start(res);
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`);
+});
